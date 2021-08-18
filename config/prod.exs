558bf7dfcb6131405393f69bb157988dcc675ac8
Teo Diaz <teo.diaz@cabify.com>
2021-08-19 00:53:27 +0200

    heroku files

--- config/prod.exs
+++ config/prod.exs
@@ -1,8 +1,10 @@
 use Mix.Config
 
 config :good_vibes, GoodVibesWeb.Endpoint,
-  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "daily-reminders.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
