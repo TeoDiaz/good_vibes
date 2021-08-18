4969050594988c8e7efe75531fe7ddbfbea43638
Teo Diaz <teo.diaz@cabify.com>
2021-08-19 01:03:15 +0200

    add elixir-buildpack

--- config/prod.exs
+++ config/prod.exs
@@ -1,6 +1,7 @@
 use Mix.Config
 
 config :good_vibes, GoodVibesWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "daily-reminders.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
