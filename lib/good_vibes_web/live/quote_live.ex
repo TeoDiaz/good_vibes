36fcda959e226827471de9511da7aa6f570c2e69
Teo Diaz <teo.diaz@cabify.com>
2021-08-20 19:02:21 +0200

    get new quote if the quote is the same as previous quote

--- lib/good_vibes_web/live/quote_live.ex
+++ lib/good_vibes_web/live/quote_live.ex
@@ -10,7 +10,7 @@ defmodule GoodVibesWeb.QuoteLive do
     Phoenix.View.render(GoodVibesWeb.QuoteView, "quote.html", assigns)
   end
 
-  defp get_random_quote do
+  defp get_random_quote() do
     case get_quotes_list() do
       %{"life_quotes" => list} -> Enum.random(list)
       _ -> "No quote today"
@@ -18,7 +18,8 @@ defmodule GoodVibesWeb.QuoteLive do
   end
 
   def handle_event("next-quote", %{"key" => " "}, socket) do
-    socket = assign(socket, :quote, get_random_quote())
+    send(self(), {:get_quote, socket.assigns})
+
     {:noreply, socket}
   end
 
@@ -29,6 +30,19 @@ defmodule GoodVibesWeb.QuoteLive do
   def handle_event("next-quote-button", _, socket) do
     socket = assign(socket, :quote, get_random_quote())
     {:noreply, socket}
+  end
+
+  def handle_info({:get_quote, %{quote: previous_quote}}, socket) do
+    case get_random_quote() do
+      next_quote when next_quote == previous_quote ->
+        send(self(), {:get_quote, previous_quote})
+
+        {:noreply, socket}
+
+      next_quote ->
+        socket = assign(socket, :quote, next_quote)
+        {:noreply, socket}
+    end
   end
 
   defp get_quotes_list do
