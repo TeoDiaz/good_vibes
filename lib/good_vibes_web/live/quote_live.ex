46bbc74b10f482faab80c5f492d684dd94cb675a
Teo Diaz <teo.diaz@cabify.com>
2021-08-22 00:59:24 +0200

    add change language option

--- lib/good_vibes_web/live/quote_live.ex
+++ lib/good_vibes_web/live/quote_live.ex
@@ -2,19 +2,43 @@ defmodule GoodVibesWeb.QuoteLive do
   use GoodVibesWeb, :live_view
 
   @quotes "priv/quotes.json"
+  @default_language "en"
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :quote, get_random_quote())}
+    {:ok,
+     assign(socket,
+       title: get_title_by_language(@default_language),
+       quote: get_random_quote(@default_language),
+       laguage: @default_language
+     )}
   end
 
   def render(assigns) do
     Phoenix.View.render(GoodVibesWeb.QuoteView, "quote.html", assigns)
   end
 
-  defp get_random_quote() do
+  defp get_random_quote(language) do
+    list_name = "#{language}_quotes"
+
     case get_quotes_list() do
-      %{"life_quotes" => list} -> Enum.random(list)
-      _ -> "No quote today"
+      %{^list_name => [_ | _] = list} -> Enum.random(list)
+      _ -> get_empty_message_by_language(language)
     end
+  end
+
+  def handle_event("es-language", _, socket) do
+    socket = assign(socket, laguage: "es", title: get_title_by_language("es"))
+
+    send(self(), {:get_quote, socket.assigns})
+
+    {:noreply, socket}
+  end
+
+  def handle_event("en-language", _, socket) do
+    socket = assign(socket, laguage: "en", title: get_title_by_language("en"))
+
+    send(self(), {:get_quote, socket.assigns})
+
+    {:noreply, socket}
   end
 
   def handle_event("next-quote", %{"key" => " "}, socket) do
@@ -33,8 +57,8 @@ defmodule GoodVibesWeb.QuoteLive do
     {:noreply, socket}
   end
 
-  def handle_info({:get_quote, %{quote: previous_quote}}, socket) do
-    case get_random_quote() do
+  def handle_info({:get_quote, %{quote: previous_quote, laguage: language}}, socket) do
+    case get_random_quote(language) do
       next_quote when next_quote == previous_quote ->
         send(self(), {:get_quote, socket.assigns})
 
@@ -51,4 +75,10 @@ defmodule GoodVibesWeb.QuoteLive do
     |> File.read!()
     |> Jason.decode!()
   end
+
+  defp get_empty_message_by_language("en"), do: "No quote today"
+  defp get_empty_message_by_language("es"), do: "Sin frases para hoy"
+
+  defp get_title_by_language("en"), do: "Your daily quote"
+  defp get_title_by_language("es"), do: "Tu frase de hoy"
 end
