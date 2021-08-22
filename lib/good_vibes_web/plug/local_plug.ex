9cda3b6cccb4383fa3a77607310e4c21d38b3491
Teo Diaz <teo.diaz@cabify.com>
2021-08-22 03:04:49 +0200

    adapt plug to fetch the locale from browser

--- lib/good_vibes_web/plug/local_plug.ex
+++ lib/good_vibes_web/plug/local_plug.ex
@@ -4,8 +4,7 @@ defmodule GoodVibesWeb.Plugs.Locale do
   def init(_opts), do: nil
 
   def call(conn, _opts) do
-    with :error <- conn |> params_locale() |> check(),
-         :error <- conn |> req_header_locale("accept-language") |> check() do
+    with :error <- conn |> req_header_locale("accept-language") |> check() do
       Gettext.put_locale(@default)
       Plug.Conn.put_session(conn, "locale", @default)
     else
@@ -26,14 +25,21 @@ defmodule GoodVibesWeb.Plugs.Locale do
     end
   end
 
-  defp params_locale(conn) do
-    conn.params["locale"]
+  defp check([h | t]) do
+    case check(h) do
+      {:ok, loc} -> {:ok, loc}
+      :error -> check(t)
+    end
   end
 
   defp check(nil), do: :error
   defp check([]), do: :error
 
   defp check(<<lang::binary-size(2)>>) do
+    {:ok, String.downcase(lang)}
+  end
+
+  defp check(<<lang::binary-size(2), "-", _region::binary-size(2)>>) do
     {:ok, String.downcase(lang)}
   end
 
